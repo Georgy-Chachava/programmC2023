e704a1d2e0f095366e9e2638ce2d8f1b830fab99
Georgy-Chachava <117064644+Georgy-Chachava@users.noreply.github.com>
2023-02-06 14:52:39 +0300

    Create B.cpp
    Problem B

---
+++ week01/B.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    int f, g, s, r;
+    if (b*b-4*a*c>0) {
+        f = (-b-sqrt(b*b-4*a*c))/(2*a);
+        g = (-b+sqrt(b*b-4*a*c))/(2*a);
+        cout << "Answer = " << f << ' ' << g << endl;
+    }
+    if (b*b-4*a*c == 0) {
+        s = (-b-sqrt(b*b-4*a*c))/(2*a);
+        cout << "Answer = " << s << endl;
+    }
+    if (b*b-4*a*c<0) {
+        cout << '\n' << endl;
+    }
+    return 0;
+}
