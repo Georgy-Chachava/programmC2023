00d09f5b4d1c13907a8919a42a190f15785626cd
Georgy-Chachava <117064644+Georgy-Chachava@users.noreply.github.com>
2023-03-08 12:27:52 +0300

    Update find_twoli.cpp

--- lab1/find_twoli.cpp
+++ lab1/find_twoli.cpp
@@ -7,10 +7,10 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void find_two(int* array, int s, int a){
-    for (int i = 0; i < a; i++){
-        for (int j = i+1; j < a; j++){
-            if (array[i]+array[j] == s){
+void find_two(int* array, int s, int a) {
+    for (int i = 0; i < a; i++) {
+        for (int j = i+1; j < a; j++) {
+            if (array[i]+array[j] == s) {
                 return;
             }
         }
@@ -18,7 +18,7 @@ void find_two(int* array, int s, int a){
 } 
 
 int main(){
-    double time1 = 0;
+    double t1 = 0;
     unsigned seed = 1001;
     int s_count = 100;
     for (int m = 100; m<2100; m += 100){
@@ -36,10 +36,10 @@ int main(){
         }
         auto end = std::chrono::steady_clock::now();
   
-        auto time_span = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-        time1 = time_span.count();
+        auto time_s = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+        t1 = time_s.count();
 
-        std::cout << time1/s_count << std::endl;
+        std::cout << t1/s_count << std::endl;
         delete [] array;
     }
 
