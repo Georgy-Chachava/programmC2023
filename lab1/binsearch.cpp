23ac3d1270ed9e0d5d1e9bf7a480da0c16903db9
Georgy-Chachava <117064644+Georgy-Chachava@users.noreply.github.com>
2023-03-08 12:24:14 +0300

    Update binsearch.cpp

--- lab1/binsearch.cpp
+++ lab1/binsearch.cpp
@@ -7,16 +7,16 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void bi_search(int* array, int k, int a){
+void bi_search(int* array, int k, int a) {
     int l = 0;
     int r = a - 1;
     int m = 0;
-    while (l < r){
+    while (l < r) {
         m = (l + r)/2;
-        if (k == array[m]){
+        if (k == array[m]) {
             return;
         }
-        if (k < array[m]){
+        if (k < array[m]) {
             r = m - 1;
         } else {
             l = m + 1;
@@ -24,8 +24,8 @@ void bi_search(int* array, int k, int a){
     }
 } 
 
-int main(){
-    double time1 = 0;
+int main() {
+    double t1 = 0;
     unsigned seed = 1001;
     int s_count = 100000;
     for (int m = 100; m<1000100; m += 50000){
@@ -43,9 +43,9 @@ int main(){
         }
         auto end = std::chrono::steady_clock::now();
   
-        auto time_span = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-        time1 = time_span.count();
-        std::cout << time1/s_count << std::endl;
+        auto time_s = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+        t1 = time_s.count();
+        std::cout << t1/s_count << std::endl;
         delete [] a;
     }
 
