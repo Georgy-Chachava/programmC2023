be00847f18cd5c2ed520381ce7c23c1e436677d7
Georgy-Chachava <117064644+Georgy-Chachava@users.noreply.github.com>
2023-03-08 12:14:42 +0300

    Update B.cpp

--- week04/B.cpp
+++ week04/B.cpp
@@ -1,46 +1,46 @@
 #include <iostream>
 
 #ifndef N
-#define N 10
+#define N 5
 #endif
 
 using namespace std;
 
-int perm(int (&mass)[N], int ind_left_base, int ind_right_base){
-    int pivot = mass[ind_right_base];
-    int ind_turn = ind_left_base;
+int per(int (&mass)[N], int ind_left_base, int ind_right_base) {
+    int p = mass[ind_right_base];
+    int ind_turn1 = ind_left_base;
 
-    for(int i = ind_left_base; i <= ind_right_base; i++){
-        if(mass[i] < pivot){
-            swap(mass[i], mass[ind_turn]);
-            ++ind_turn;
+    for (int i = ind_left_base; i <= ind_right_base; i++) {
+        if (mass[i] < p) {
+            swap(mass[i], mass[ind_turn1]);
+            ++ind_turn1;
         }
     }
-    swap(mass[ind_turn], mass[ind_right_base]);
-    return ind_turn;
+    swap(mass[ind_turn1], mass[ind_right_base]);
+    return ind_turn1;
 }
 
-void qsort(int (&mass)[N], int ind_left_base, int ind_right_base){
-    if(ind_left_base >= ind_right_base){ 
+void qsort(int (&mass)[N], int ind_left_base, int ind_right_base) {
+    if (ind_left_base >= ind_right_base) { 
         
         return;
     
     }
     
-    int pivot = perm(mass, ind_left_base, ind_right_base);
+    int p = per(mass, ind_left_base, ind_right_base);
     
-    qsort(mass, ind_left_base, pivot - 1);
-    qsort(mass, pivot + 1, ind_right_base);
+    qsort(mass, ind_left_base, p - 1);
+    qsort(mass, p + 1, ind_right_base);
 }
 
-int main(){
+int main() {
     int array[N];
-    for(int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++) {
         cin >> array[i];
     }
     qsort(array, 0, N-1);
     
-    for(int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++) {
         cout << array[i] << " ";
     }
     cout << endl;
