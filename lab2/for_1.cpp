75aceadb452cd71c2443380ef85313ab08d86d02
Georgy-Chachava <117064644+Georgy-Chachava@users.noreply.github.com>
2023-03-15 15:39:05 +0300

    Create for_1.cpp

---
+++ lab2/for_1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#ifndef N
+#define N 5  
+#endif
+
+void swap(unsigned array[], int i) {
+    int p;
+    p = array[i];
+    array[i] = array[i-1];
+    array[i-1] = p;
+}
+
+void print_array(unsigned array[], int n) {
+    for (int i = 0; i<n; ++i) {
+        cout << array[i] << ' ';
+    }
+    cout << endl;
+}
+
+void forward_step(unsigned arr[], unsigned const begin_idx, unsigned const end_idx) {
+    for (unsigned i = begin_idx; i < end_idx; i++) {
+        if (arr[i] > arr[i+1]) {
+            swap(arr, i+1);
+        }
+    }
+}
+
+int main() {
+    int b = 0;
+    cin >> b;
+    int e = 0;
+    cin >> e;
+    unsigned array[N];
+    for (int j = N; j > 0; j--) {
+        array[N-j] = j-1;
+    }
+    print_array(array, N);
+    forward_step(array, b, e);
+    print_array(array, N);
+    return 0;
+}
