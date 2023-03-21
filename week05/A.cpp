01226c5f31e241221c85f6eef763ce0b497721ab
Georgy-Chachava <117064644+Georgy-Chachava@users.noreply.github.com>
2023-03-21 16:59:17 +0300

    Update A.cpp

--- week05/A.cpp
+++ week05/A.cpp
@@ -6,4 +6,5 @@ int main() {
     std::cout << ptr << ' ' << *ptr << '\n';
     std::cout << ptr - 3 << ' ' << *(ptr - 3) << '\n';
     std::cout << ptr + 5 << ' ' << *(ptr + 5) << '\n';
+    return 0;
 }
