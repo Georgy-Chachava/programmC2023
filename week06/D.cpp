c13e23c5e6273d8295ae98752e401c7b639f03a9
Georgy-Chachava <117064644+Georgy-Chachava@users.noreply.github.com>
2023-03-27 22:04:16 +0300

    Update D.cpp

--- week06/D.cpp
+++ week06/D.cpp
@@ -13,3 +13,31 @@ void swap(int l, int r) {
     l = r;
     r = tmp;
 }
+
+int main() {
+    int a = 0;
+    std::cin >> s1.b_x >> s1.b_y >> s1.e_x >> s1.e_y >> s2.b_x >> s2.b_y >> s2.e_x >> s2.e_y;
+    if (s1.b_x > s1.e_x) {
+        swap(s1.b_x, s1.e_x);
+    }
+    if (s1.b_y > s1.e_y) {
+        swap(s1.b_y, s1.e_y);
+    }
+    if (s2.b_x > s2.e_x) {
+        swap(s2.b_x, s2.e_x);
+    }
+    if (s2.b_y > s2.e_y) {
+        swap(s2.b_y, s2.e_y);
+    }
+    if (s1.b_x < s2.e_x and s1.e_x > s2.b_x and s1.b_y < s2.e_y and s1.e_y > s2.b_y) {
+        a = -1;
+    } else {
+        a = 1;
+    }
+    if (a == -1) {
+        std::cout << "NO" << std::endl;
+    } else {
+        std::cout << "YES" << std::endl;
+    }
+    return 0;
+}
